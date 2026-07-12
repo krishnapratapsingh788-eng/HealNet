a8743d700b253143f32bc6d03834521de5d01b73
Krishna Pratap Singh <krishnapratapsingh788@gmail.com>
2026-07-12 19:48:38 +0530

    Secure database credentials using env

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,17 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
+
     conn = psycopg2.connect(
-        host="localhost",
-        database="healnet",
-        user="postgres",
-        password="healnet123",
-        port="5432"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
+
     return conn
